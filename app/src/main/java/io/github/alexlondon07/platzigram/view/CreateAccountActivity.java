8f74717900d28ef992596fd5fd38aacab43ee696
Alexander Londoño Espejo <alexlondon07@secuencia24.com>
2017-09-10 21:43:31 -0500

    first commit

---
+++ app/src/main/java/io/github/alexlondon07/platzigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package io.github.alexlondon07.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.alexlondon07.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
