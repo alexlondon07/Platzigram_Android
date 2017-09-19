8c48f981ea32118e72b1d8cd6fd489560c8f0e6a
Alexander Londoño Espejo <alexlondon07@secuencia24.com>
2017-09-18 22:58:58 -0500

    Toolbar

--- app/src/main/java/io/github/alexlondon07/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/io/github/alexlondon07/platzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package io.github.alexlondon07.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import io.github.alexlondon07.platzigram.R;
 
@@ -11,5 +12,17 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolar_title_createaccount), false);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*Titulo de toolbar*/
+        getSupportActionBar().setTitle(title);
+
+        /*En caso que tenga Boton de regreso*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
